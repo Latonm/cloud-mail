90a565b4eaa3512da1702f4bb467e3ba42a189c8
Latonm <155351544+Latonm@users.noreply.github.com>
2026-04-30 19:17:55 +0800

    Add zh-TW language support to i18n
    Added Traditional Chinese language support to i18n.

--- mail-vue/src/i18n/index.js
+++ mail-vue/src/i18n/index.js
@@ -1,11 +1,14 @@
 import { createI18n } from 'vue-i18n';
-import en from './en.js'
-import zh from './zh.js'
+import en from './en.js';
+import zh from './zh.js';
+import zhTW from './zh-TW.js'; // 1. 匯入繁體中文語言檔
+
 const i18n = createI18n({
     legacy: false,
     messages: {
         zh,
-        en
+        en,
+        'zh-TW': zhTW // 2. 加入到 messages 資源物件中
     },
 });
 

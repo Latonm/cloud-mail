9d7f129bc9f9875ef7c01a425fb3396dcae03a86
Latonm <155351544+Latonm@users.noreply.github.com>
2026-04-30 23:57:46 +0800

    Update default language to 'zh-TW' in i18n.js
    Change default language from 'zh' to 'zh-TW' and update resources accordingly.

--- mail-worker/src/i18n/i18n.js
+++ mail-worker/src/i18n/i18n.js
@@ -1,12 +1,12 @@
 import i18next from 'i18next';
-import zh from './zh.js';
 import en from './en.js';
 import zhTW from './zhTW.js';
 import app from '../hono/hono';
 
 app.use('*', async (c, next) => {
 	const headerLang = c.req.header('accept-language');
-	const lang = headerLang ? headerLang.split(',')[0].split(';')[0] : 'zh';
+	// 將預設語言從 'zh' 改為 'zh-TW'
+	const lang = headerLang ? headerLang.split(',')[0].split(';')[0] : 'zh-TW';
 	
 	i18next.init({
 		lng: lang,
@@ -18,16 +18,13 @@ const resources = {
 	en: {
 		translation: en
 	},
-	zh: {
-		translation: zh,
-	},
-	'zh-TW': { // 語言代碼的 Key 必須要有連字號，瀏覽器才認得
-		translation: zhTW, // 這裡的值才是剛才匯入且沒有連字號的變數名稱
+	'zh-TW': {
+		translation: zhTW,
 	},
 };
 
 i18next.init({
-	fallbackLng: 'zh-TW', // 後備語言代碼也必須要有連字號
+	fallbackLng: 'zh-TW',
 	resources,
 });
 

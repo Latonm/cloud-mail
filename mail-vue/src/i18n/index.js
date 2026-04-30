f8870cdeed0d201b36bf6f8d94c0e91fcfb77bc1
Latonm <155351544+Latonm@users.noreply.github.com>
2026-04-30 20:01:53 +0800

    Fix zhTW key format in i18n messages

--- mail-vue/src/i18n/index.js
+++ mail-vue/src/i18n/index.js
@@ -1,14 +1,14 @@
 import { createI18n } from 'vue-i18n';
 import en from './en.js';
 import zh from './zh.js';
-import zhTW from './zhTW.js'; // 1. 匯入繁體中文語言檔
+import zhTW from './zhTW.js'; // 1. 匯入繁體中文語言檔 (變數名稱沒有連字號)
 
 const i18n = createI18n({
     legacy: false,
     messages: {
         zh,
         en,
-        'zhTW': zhTW // 2. 加入到 messages 資源物件中
+        'zh-TW': zhTW // 2. 這裡的 Key 必須加上連字號，改成 'zh-TW'
     },
 });
 
